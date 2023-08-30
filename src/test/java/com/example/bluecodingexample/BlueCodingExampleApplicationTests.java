14ef3e1779d748dd1b22f0c8c972227b486020bd
Evgenii Ivanov <eugene.ivanov@sporty.com>
2023-08-30 15:49:40 -0300

    Initial commit

---
+++ src/test/java/com/example/bluecodingexample/BlueCodingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bluecodingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueCodingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
